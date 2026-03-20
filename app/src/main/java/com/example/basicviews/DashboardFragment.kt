3abe3927e17da19c3b78746ec1c2147cac8d6707
AnamXI <anamggss1227@gmail.com>
2026-03-21 00:13:17 +0800

    Category Cards & Tracker

--- app/src/main/java/com/example/basicviews/DashboardFragment.kt
+++ app/src/main/java/com/example/basicviews/DashboardFragment.kt
@@ -23,6 +23,11 @@ class DashboardFragment : Fragment() {
     private val allItems = mutableListOf<BakeryItem>() // All items
     private val displayedItems = mutableListOf<BakeryItem>() // What will be displayed, gets all if walang search
 
+    //For the category cards & toggle. Tracks active catcard & sets color.
+    private var activeCategoryCard: androidx.cardview.widget.CardView? = null
+    private val activeColor = android.graphics.Color.LTGRAY
+    private val inactiveColor = android.graphics.Color.WHITE
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -41,17 +46,34 @@ class DashboardFragment : Fragment() {
         setupRecyclerView()
         fetchInventoryData()
 
+        //Monitors text on searchbox & updates the filter per character
         binding.searchView.setOnQueryTextListener(object : android.widget.SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return false
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
-                // monitors changes per letter & then updates
                 filterList(newText)
                 return true
             }
         })
+
+        //For the category cards
+        binding.categoryBreads.setOnClickListener {
+            toggleCategory(binding.categoryBreads, "Breads")
+        }
+
+        binding.categoryCakes.setOnClickListener {
+            toggleCategory(binding.categoryCakes, "Cakes")
+        }
+
+        binding.categoryPastries.setOnClickListener {
+            toggleCategory(binding.categoryPastries, "Pastries")
+        }
+
+        binding.categoryCookies.setOnClickListener {
+            toggleCategory(binding.categoryCookies, "Cookies")
+        }
     }
 
     private fun setupRecyclerView() {
@@ -68,7 +90,7 @@ class DashboardFragment : Fragment() {
                     val item = itemSnapshot.getValue(BakeryItem::class.java)
                     item?.let { allItems.add(it) }
                 }
-                // Initially show all items, then when user puts chars in search, lcive update
+                // Initially show all items, then when user puts chars in search, lkive update
                 filterList(binding.searchView.query.toString())
             }
 
@@ -83,6 +105,7 @@ class DashboardFragment : Fragment() {
         _binding = null
     }
 
+    //For the search filter
     private fun filterList(query: String?) {
         val filteredList = if (query.isNullOrEmpty()) {
             allItems // Show everything if search is empty
@@ -93,5 +116,23 @@ class DashboardFragment : Fragment() {
             }
         }
         adapter.updateList(filteredList)
+    }
+
+    //For the category toggle & filter
+    private fun toggleCategory(selectedCard: androidx.cardview.widget.CardView, category: String) {
+        if (activeCategoryCard == selectedCard) {
+            selectedCard.setCardBackgroundColor(inactiveColor)
+            activeCategoryCard = null
+            binding.searchView.setQuery("", false)
+            filterList("") // Show all
+        } else {
+            activeCategoryCard?.setCardBackgroundColor(inactiveColor)
+
+            selectedCard.setCardBackgroundColor(activeColor)
+            activeCategoryCard = selectedCard
+
+            binding.searchView.setQuery(category, false)
+            filterList(category)
+        }
     }
 }
